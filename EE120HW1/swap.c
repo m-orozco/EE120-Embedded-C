87890806b26af0c1051259a6a81c689efbc5ee01
Martin Orozco <morozc91@gmail.com>
2026-09-02 22:29:08 -0700

    Part 1 a and b complete

--- EE120HW1/swap.c
+++ EE120HW1/swap.c
@@ -1,8 +1,11 @@
-/* calling a Swap from main() */
+/* calling a Swap from main()
 int x = 42, y = 17;
-Swap(&x, &y);
+Swap(&x, &y); */
 
-/* correct version of Swap */
+#include <stdio.h> // header file for printf
+
+/* correct version of Swap, uses pointers to int, 
+holds addresses of x and y */
 void Swap(int *px, int *py)
 {
     int temp;
@@ -10,5 +13,43 @@ void Swap(int *px, int *py)
     *px = *py;
     *py = temp;
 
+    // Debugging output to show the values inside the Swap function
     printf("Inside Swap: x = %d, y = %d\n", *px, *py);
+}
+
+/* Swap with three variables
+    x to y, y to z*/
+void SwapXYZ(int *px, int *py, int *pz)
+{
+    int tempx;
+    tempx = *px; //original x value
+    int tempy;
+    tempy = *py; //original z value
+    *px = *pz;
+    *py = tempx;
+    *pz = tempy;
+    printf("Inside SwapXYZ: x = %d, y = %d, z = %d\n", *px, *py, *pz);
+
+}
+
+int main(void)
+{
+    /* Part a: Swap function with x=42 and y=17, output should 
+        be x = 17, y = 42 */
+    int x = 42, y = 17;
+    printf("\nHomework 1: Pointer-Based Swap Function\n### Part a: x,y ###\n");
+    printf("Before Swap: x = %d, y = %d\n", x, y);
+    Swap(&x, &y); // Call Swap passing addresses(&) of x and y
+    printf("After Swap: x = %d, y = %d\n", x, y);
+
+    /* Part b: Swap function with x = 10, y = 21, and z = 32
+        should return x = 32, y = 10, and z = 21. */
+    int x2 = 10, y2 = 21, z = 32;
+    printf("\n### Part b: x = 10, y = 21, z = 32 ###\n"); 
+    printf("should return x = 32, y = 10, and z =32\n");
+    printf("Before Swap: x = %d, y = %d, z = %d\n", x2, y2, z);
+    SwapXYZ(&x2, &y2, &z);
+    printf("After Swap: x = %d, y = %d, z = %d\n", x2, y2, z);
+
+    return 0;
 }
